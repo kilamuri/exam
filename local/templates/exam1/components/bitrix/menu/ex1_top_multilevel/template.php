7f47658e89f1a0bb58833def67f8e4f4e96d1f09
Екатерина Кантемирова <kantemirova@intervolga.ru>
2024-06-24 21:52:54 +0300

    Текст в параметрах меню

--- local/templates/exam1/components/bitrix/menu/ex1_top_multilevel/template.php
+++ local/templates/exam1/components/bitrix/menu/ex1_top_multilevel/template.php
@@ -1,5 +1,5 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-$previousLevel = 0;?>
+$previousLevel = 0; ?>
 <?php if ($arResult):?>
 <nav class="nav">
 <div class="inner-wrap">
@@ -15,28 +15,31 @@ $previousLevel = 0;?>
 		<?php if ($arItem["DEPTH_LEVEL"] == 1):?>
 				<li><a href="<?=$arItem["LINK"]?>" class=""><?=$arItem["TEXT"]?></a>
 					<ul>
+						<?php if ($arItem['PARAMS']['DESCRIPTION']): ?>
+							<div class="menu-text"><?=$arItem['PARAMS']['DESCRIPTION']?></div>
+						<?php endif ?>
 		<?php else:?>
+						<?php if ($arItem['PARAMS']['DESCRIPTION']): ?>
+							<div class="menu-text"><?=$arItem['PARAMS']['DESCRIPTION']?></div>
+						<?php endif ?>
 				<li class=""><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
 					<ul>
 		<?php endif?>
 	<?php else:?>
 		<?php if ($arItem["PERMISSION"] > "D"):?>
 			<?php if ($arItem["DEPTH_LEVEL"] == 1):?>
+				<?php if ($arItem['PARAMS']['DESCRIPTION']): ?>
+					<div class="menu-text"><?=$arItem['PARAMS']['DESCRIPTION']?></div>
+				<?php endif ?>
 				<li class="<?php if($arItem['LINK'] == '/'): ?>main-page<?php endif?>"><a href="<?=$arItem["LINK"]?>" class=""><?=$arItem["TEXT"]?></a></li>
 			<?php else:?>
 				<li class=""><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
 			<?php endif?>
-<!--		--><?php //else:?>
-<!--			--><?php //if ($arItem["DEPTH_LEVEL"] == 1):?>
-<!--				<li><a href="" class="--><?php //if ($arItem["SELECTED"]):?><!--root-item-selected--><?php //else:?><!--root-item--><?php //endif?><!--" title="--><?php //=GetMessage("MENU_ITEM_ACCESS_DENIED")?><!--">--><?php //=$arItem["TEXT"]?><!--</a></li>-->
-<!--			--><?php //else:?>
-<!--				<li><a href="" class="denied" title="--><?php //=GetMessage("MENU_ITEM_ACCESS_DENIED")?><!--">--><?php //=$arItem["TEXT"]?><!--</a></li>-->
-<!--			--><?php //endif?>
 		<?php endif?>
 	<?php endif?>
 	<?php $previousLevel = $arItem["DEPTH_LEVEL"];?>
 <?php endforeach?>
-	<?php if ($previousLevel > 1)://close last item tags?>
+	<?php if ($previousLevel > 1): ?>
 	<?=str_repeat("</ul></li>", ($previousLevel-1) );?>
 	<?php endif?>
 				</ul>
